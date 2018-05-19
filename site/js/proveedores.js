d801dff9dcf9b1cfa2b71137c03b8234e67cac9d
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 01:26:14 -0500

    JavaScript que contiene la funcionalidad del módulo Proveedores

---
+++ site/js/proveedores.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var url="http://127.0.0.1/inventarios/database/read/proveedores.php";
+	$.getJSON(url,function(result){
+		console.log(result);
+		$.each(result, function(i, field){
+			var id=field.id;
+			var proveedor=field.proveedor;
+			var ciudad=field.ciudad;
+			var direccion=field.direccion;
+			var telefono=field.telefono;
+			var nit=field.nit;
+			$("#proveedoresLista").append("<tr><td>"+proveedor+"</td><td>"+ ciudad + "</td><td>"+ direccion +"</td><td>"+ telefono +"</td><td>"+ nit +"</td><td><a href='form.html?id="+id+"&proveedor="+proveedor+"&ciudad="+ciudad+"&direccion="+direccion+"&telefono="+telefono+"&nit="+nit+"'>Actualizar</a></td></tr>");
+		});
+	});
+});
