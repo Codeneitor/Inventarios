046655323f9e5d2495095e3f299c70a68f867079
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 00:32:35 -0500

    Archivo que contiene el menú de módulos disponibles

---
+++ site/menu.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<div class="panel-menu">
+            <table class="tabla-menu degrade">
+                <tr>
+                    <td>
+                        <div id="proveedores">Proveedores</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div id="compras">Compras</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div id="inventarios">Inventario</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div id="clientes">Clientes</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div id="ventas">Ventas</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div id="admin">Administrados</div>
+                    </td>
+                </tr>
+            </table>
+        </div>
