fca6299abd24ea03aa0ba7a311b1bdd5c5b1ce16
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 01:49:45 -0500

    Módulo Proveedores

---
+++ site/modulos/proveedores.php
@@ -0,0 +1,13 @@
+<script src="./js/proveedores.js"></script>
+<div class="panel-modulo" id="panelModulo">
+			<table id="proveedoresLista" class="table-modulo list" border="1">
+			    <tr>
+			        <th width="60%" style="text-align: left;">Proveedor</th>
+			        <th width="10%">Ciudad</th>
+			        <th width="10%">Dirección</th>
+			        <th width="10%">Teléfono</th>
+			        <th width="10%">Nit</th>
+			        <th width="10%">Actualizar</th>
+			    </tr>
+			</table>
+		</div>
