5316516c064e248938eeb87aed5feaff3afc91bd
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 00:13:49 -0500

    Página principal

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<?php  include("imports.php"); ?>
+	<title>Sistema de inventarios.</title>
+</head>
+<body>
+<div class="aplicacion">
+	<?php  include("header.php"); echo"\n";?>
+	<?php include("navbar.php"); echo"\n";?>
+	<h1>Inventarios</h1>
+	<div class="panel-main">
+		<?php include("menu.php"); echo"\n";?>
+		<?php include("modulo.php"); echo"\n";?>
+	</div>
+	<?php
+	echo"\n";
+	?>
+</div>
+<?php  include("footer.php");?>
