06ac1c5748fe03604aa0b8bca51a75f0fd637b6b
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 02:08:18 -0500

    Script PHP que realiza la instalación de la base de datos.

---
+++ server/install/install_db.php
@@ -0,0 +1,120 @@
+<?php
+
+//ESTA CONEXIÓN SERÁ REEMPLAZADA POR UNA DINÁMICA
+//LOS PARÁMETROS LOS RECIBIREMOS POR _POST
+$db="GestionInventario";
+$conexion=mysqli_connect("localhost","Programador","programador");
+if(!$conexion){
+	die("Error de conexion:<br>".mysqli_error());
+}else{
+	if(mysqli_query($conexion,"CREATE DATABASE $db")){
+		echo"Se ha creado la base de datos<br>";
+		//SELECCIONAMOS LA BASE DE DATOS
+		mysqli_select_db($conexion,$db);
+		//INICIAMOS LA CREACIÓN DE LAS TABLAS
+		//TABLA proveedores
+		$sqlProveedores="CREATE TABLE proveedores(
+		proveedor_id INT(11) NOT NULL AUTO_INCREMENT,
+		proveedor VARCHAR(50) NOT NULL,
+		ciudad VARCHAR(25) NOT NULL,
+		direccion VARCHAR(50) NOT NULL,
+		telefono VARCHAR(15) NOT NULL,
+		nit VARCHAR(25) NOT NULL,
+		PRIMARY KEY(proveedor_id)) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+		//ENVIAMOS LA CONSULTA
+		if(mysqli_query($conexion,$sqlProveedores)){
+			echo"La tabla Proveedores ha sido creada.<br>";
+			//TABLA proveedores
+			$sqlFacturaCompras="CREATE TABLE factura_compras(
+			factura_compra_id INT(11) NOT NULL AUTO_INCREMENT,
+			proveedor_id INT(11) NOT NULL,
+			factura INT(11) NOT NULL,
+			fecha_compra DATE NOT NULL,
+			PRIMARY KEY (factura_compra_id),
+			INDEX prov_ind (proveedor_id),
+			FOREIGN KEY(proveedor_id) REFERENCES proveedores(proveedor_id) ON DELETE CASCADE
+			) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+			//ENVIAMOS LA CONSULTA
+			if(mysqli_query($conexion,$sqlFacturaCompras)){
+				echo"La tabla factura_compras ha sido creada.<br>";
+				$sqlProductos ="CREATE TABLE productos(
+				producto_id INT(11) NOT NULL AUTO_INCREMENT,
+				referencia VARCHAR(25) NOT NULL,
+				descripcion VARCHAR(50) NOT NULL,
+				tamano VARCHAR(25) NOT NULL,
+				color VARCHAR(25) NOT NULL,
+				costo INT(11) NOT NULL,
+				precio INT(11) NOT NULL,
+				PRIMARY KEY (producto_id),
+				proveedor_id INT(11) NOT NULL,
+				INDEX prov_ind (proveedor_id),
+				FOREIGN KEY(proveedor_id) REFERENCES proveedores(proveedor_id) ON DELETE CASCADE,
+				factura_compra_id INT(11) NOT NULL,
+				INDEX factura_ind (factura_compra_id),
+				FOREIGN KEY(factura_compra_id) REFERENCES factura_compras(factura_compra_id) ON DELETE CASCADE
+				) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+				if(mysqli_query($conexion,$sqlProductos)){
+					echo"La tabla productos ha sido creada.<br>";
+					$sqlClientes="CREATE TABLE clientes(
+					cliente_id INT(11) NOT NULL AUTO_INCREMENT,
+					nombres VARCHAR(25) NOT NULL,
+					apellidos VARCHAR(25) NOT NULL,
+					documento VARCHAR(15) NOT NULL,
+					phone VARCHAR(15),
+					celular VARCHAR(15),
+					PRIMARY KEY (cliente_id)
+					) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+					if(mysqli_query($conexion,$sqlClientes)){
+						echo"La tabla clientes ha sido creada.<br>";
+						$sqlFacturaClientes="CREATE TABLE factura_clientes(
+						factura_cliente_id INT(11) NOT NULL AUTO_INCREMENT,
+						PRIMARY KEY (factura_cliente_id),
+						cliente_id INT(11) NOT NULL,
+						INDEX clie_ind (cliente_id),
+						FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id) ON DELETE CASCADE,
+						producto_id INT(11) NOT NULL,
+						INDEX prod_ind(producto_id),
+						FOREIGN KEY (producto_id) REFERENCES productos(producto_id) ON DELETE CASCADE,
+						cantidad INT(6) NOT NULL
+						) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+						if(mysqli_query($conexion,$sqlFacturaClientes)){
+							echo "La tabla factura_clientes ha sido creada.<br>";
+							$sqlInventario="CREATE TABLE inventario(
+							inventario_id INT(11) NOT NULL AUTO_INCREMENT,
+							PRIMARY KEY (inventario_id),
+							producto_id INT(11) NOT NULL,
+							INDEX prod_ind (producto_id),
+							FOREIGN KEY(producto_id) REFERENCES productos(producto_id) ON DELETE CASCADE,
+							proveedor_id INT(11) NOT NULL,
+							INDEX prov_ind (proveedor_id),
+							FOREIGN KEY(proveedor_id) REFERENCES proveedores(proveedor_id) ON DELETE CASCADE,
+							quantity INT(11) NOT NULL,
+							factura_compra_id INT(11) NOT NULL,
+							INDEX invo_ind (factura_compra_id),
+							FOREIGN KEY(factura_compra_id) REFERENCES factura_compras(factura_compra_id) ON DELETE CASCADE
+							) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+							if(mysqli_query($conexion,$sqlInventario))
+								echo "La tabla inventario ha sido creada.<h2>Instalación Finalizada</h2>", include("index.php");
+							else
+								echo "No se ha podido crear la tabla inventario";
+						}else{
+							echo "No se ha podido crear la tabla factura_clientes";
+						}
+					}else{
+						echo "No se ha podido crear la tabla clientes";
+					}
+				}else{
+					echo "No se ha podido crear la tabla productos";
+				}
+			}else{
+				echo "No se ha podido crear la tabla factura_compras".mysqli_error();
+			}
+		}else{
+			echo "No se ha podido crear la tabla proveedores".mysqli_error();
+		}
+	}else{
+		echo"Error de INSTALACIÓN:<br>".mysqli_error();
+	}
+}
+mysqli_close($conexion);
+?>
