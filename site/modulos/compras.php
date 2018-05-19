2ecb69bf12d01dd345953e452d5e759cbd4922a8
ColProgSoft <bnovoa.linux@gmail.com>
2018-05-19 01:49:14 -0500

    Módulo Compras

---
+++ site/modulos/compras.php
@@ -0,0 +1,10 @@
+<script src="./js/compras.js"></script>
+		<div class="panel-modulo" id="panelModulo">
+			<table id="comprasLista" class="table-modulo" border="1">
+			    <tr>
+			        <th width="60%" style="text-align: left;">Proveedor</th>
+			        <th width="10%">N°Factura</th>
+			        <th width="10%">Fecha</th>
+			    </tr>
+			</table>
+		</div>
